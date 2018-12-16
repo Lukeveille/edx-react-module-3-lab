e5b08ca3ec1bebe4be6d853b8398a9f33f1b153c
Lukeveille <luke@lukeleveille.com>
2018-12-15 19:34:28 -0500

    form is linked to react state and updating properly

--- App.js
+++ App.js
@@ -13,14 +13,42 @@ function ActivitySelect(props) {
     {activities}
   </select>
 }
+function Restrictions(props) {
+  let restrictions = Object.keys(props.restrictions)
+  let restrictionList = restrictions.map((restriction, index) => {
+    console.log(restriction)
+    console.log(index)
+
+    return <Restriction key={index}
+      letter={String.fromCharCode(index+97)}
+      restriction={restriction}
+      handleChange={props.handleChange}
+      checked={props.restrictions[restriction]}
+    />
+  })
+  return <div>
+    <p>Check all that apply</p>
+    {restrictionList}
+  </div>
+}
+function Restriction(props) {
+  return <div>
+    <input type="checkbox" checked={props.checked} value={props.restriction} onChange={e => props.handleChange(e)} /> {props.letter}) {props.restriction}
+  </div>
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      first: 'Luke',
-      last: 'Leveille',
+      first: '',
+      last: '',
       activity: 'Science Lab',
+      restrictions: {
+        'Dietary Restrictions': true,
+        'Physical Disabilities': false,
+        'Medical Needs': false,
+      }
     }
   }
   changeFirst(e) {
@@ -32,12 +60,18 @@ class App extends React.Component {
   changeActivity(e) {
     this.setState({activity: e.target.value})
   }
+  changeRestrictions(e) {
+    let restrictions = this.state.restrictions
+    restrictions[e.target.value] = !this.state.restrictions[e.target.value]
+    this.setState({restrictions})
+  }
   
   render() {
     return <div>
       <NameField name='First' nameValue={this.state.first} handleChange={this.changeFirst.bind(this)} />
       <NameField name='Last' nameValue={this.state.last} handleChange={this.changeLast.bind(this)} />
       <ActivitySelect activities={['Science Lab', 'Cooking', 'Painting', 'Swimming', 'Coding', 'Biking']} handleChange={this.changeActivity.bind(this)} />
+      <Restrictions restrictions={this.state.restrictions} handleChange={this.changeRestrictions.bind(this)}/>
     </div>
   }
 }
